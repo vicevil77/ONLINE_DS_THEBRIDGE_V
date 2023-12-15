a44d3cafd27c634163306772f5113ca49a9b4463
Víctor M. Martínez <victormbards@gmail.com>
2023-12-15 18:03:36 +0100

    1

--- proyecto EDA/geo_IP3.py
+++ proyecto EDA/geo_IP3.py
@@ -1,10 +1,13 @@
-import requests
+
 import pandas as pd
 import folium
 from folium.plugins import PolyLineTextPath
+from geoip2.database import Reader
 
-# Reemplaza 'TU_API_KEY' con tu propia clave de API de positionstack
-API_KEY = '7acfe9588146c079e834a9742a62fce6'
+
+
+# Ruta al archivo GeoLite2 City Database (descárgalo desde https://dev.maxmind.com/geoip/geoip2/geolite2/)
+GEOIP_DATABASE_PATH = r"E:\Cursos\BC_Data_Science\Repositorio\ONLINE_DS_THEBRIDGE_V\proyecto EDA\BBDD geoloca\GeoLite2-City_20231212\GeoLite2-City.mmdb"  # Reemplaza con la ruta correcta
 
 # Cargar el conjunto de datos
 df = pd.read_excel(r'E:\Cursos\BC_Data_Science\Repositorio\ONLINE_DS_THEBRIDGE_V\proyecto EDA\DATASETS\DATASETS MODIFICADOS\ciber_modificado2.xlsx')
@@ -12,46 +15,35 @@ df = pd.read_excel(r'E:\Cursos\BC_Data_Science\Repositorio\ONLINE_DS_THEBRIDGE_V
 # Crear una lista vacía para guardar las coordenadas de las IPs
 coords = []
 
+# Crear el objeto Reader para acceder a la base de datos de GeoLite2 City
+geoip_reader = Reader(GEOIP_DATABASE_PATH)
+
 # Iterar sobre las filas del dataset y obtener las coordenadas de cada IP
 for index, row in df.iterrows():
-    # Obtener la IP origen y la IP destino
-    ip_origen = row["IP Origen"]
-    ip_destino = row["Destino IP"]
+    ip_origen = row["Destino IP"]
+    ip_destino = row["IP Origen"]
 
-    # Hacer una petición a positionstack para obtener la información de geolocalización de la IP origen
-    url_origen = f"http://api.positionstack.com/v1/forward?access_key={API_KEY}&query={ip_origen}"
-    response_origen = requests.get(url_origen)
-    data_origen = response_origen.json()
+    try:
+        # Obtener la información de geolocalización de la IP origen
+        response_origen = geoip_reader.city(ip_origen)
+        lat_origen, lon_origen = response_origen.location.latitude, response_origen.location.longitude
 
-    if not data_origen["data"]:
-        print(f"No se encontraron resultados para la IP origen: {ip_origen}")
-        continue
+        # Obtener la información de geolocalización de la IP destino
+        response_destino = geoip_reader.city(ip_destino)
+        lat_destino, lon_destino = response_destino.location.latitude, response_destino.location.longitude
 
-    lat_origen = data_origen["data"][0]["latitude"]
-    lon_origen = data_origen["data"][0]["longitude"]
+        coords.append([(lat_destino, lon_destino), (lat_origen, lon_origen)])
 
-    # Hacer lo mismo con la IP destino
-    url_destino = f"http://api.positionstack.com/v1/forward?access_key={API_KEY}&query={ip_destino}"
-    response_destino = requests.get(url_destino)
-    data_destino = response_destino.json()
-
-    if not data_destino["data"]:
-        print(f"No se encontraron resultados para la IP destino: {ip_destino}")
-        continue
-
-    lat_destino = data_destino["data"][0]["latitude"]
-    lon_destino = data_destino["data"][0]["longitude"]
-
-    # Añadir las coordenadas de la IP origen y la IP destino a la lista coords
-    coords.append([(lat_origen, lon_origen), (lat_destino, lon_destino)])
+    except Exception as e:
+        print(f"No se encontraron resultados para la IP: {ip_origen} o {ip_destino}. Error: {e}")
 
 # Crear un mapa de Folium
 m = folium.Map(location=[0, 0], zoom_start=2)
 
 # Pintar las IPs con puntos usando la función Marker de Folium
 for coord_pair in coords:
-    folium.Marker(coord_pair[0], icon=folium.Icon(color='blue'), popup='IP Origen').add_to(m)
     folium.Marker(coord_pair[1], icon=folium.Icon(color='red'), popup='Destino IP').add_to(m)
+    folium.Marker(coord_pair[0], icon=folium.Icon(color='blue'), popup='IP Origen').add_to(m)
 
 # Unir las IPs origen con las de destino con líneas usando PolyLineTextPath
 for coord_pair in coords:
