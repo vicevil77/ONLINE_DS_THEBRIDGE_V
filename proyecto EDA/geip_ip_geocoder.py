e0cfa46dec54ad90322c804736b35c981a6c82b5
Víctor M. Martínez <victormbards@gmail.com>
2023-12-18 09:16:10 +0100

    EDA

--- proyecto EDA/geip_ip_geocoder.py
+++ proyecto EDA/geip_ip_geocoder.py
@@ -1,17 +1,11 @@
-# -*- coding: utf-8 -*-
-import ipwhois
 import time
-import folium
-from folium.plugins import PolyLineTextPath
-import pandas as pd
 import requests
+import folium
+import ipinfo
+import pandas as pd
 
 
-# Cargar el conjunto de datos
-df = pd.read_excel(r'E:\Cursos\BC_Data_Science\Repositorio\ONLINE_DS_THEBRIDGE_V\proyecto EDA\DATASETS\DATASETS MODIFICADOS\ciber_modificado.xlsx')
-
-# Define tu clave de API de ipstack
-api_key = '5dbd572c7b96c5029e04f62e98d19e8f'
+df_modificado = pd.read_excel(r'E:\Cursos\BC_Data_Science\Repositorio\ONLINE_DS_THEBRIDGE_V\proyecto EDA\DATASETS\DATASETS MODIFICADOS\ciber_modificado.xlsx')
 
 # Crear una lista vacía para guardar las coordenadas de las IPs
 coords = []
@@ -19,20 +13,21 @@ coords = []
 # Definir la cantidad máxima de IPs a procesar en cada iteración (10,000 en este caso)
 max_ips_per_iteration = 10000
 
+# Configurar tu token de acceso a la API de IPinfo
+api_token = 'c97bf94b717fc3'  # Reemplaza 'tu_api_token' con tu token de API de IPinfo
+
+# Inicializar el cliente de IPinfo
+ipinfo_handler = ipinfo.getHandler(api_token)
+
 # Iterar sobre las filas del dataset y obtener las coordenadas de cada IP
-for index, row in df.iterrows():
+for index, row in df_modificado.iterrows():
     ip_origen = row["Destino IP"]
     ip_destino = row["IP Origen"]
 
     try:
-        # Utilizar la API de ipstack para obtener información de geolocalización
-        api_url_destino = f"http://api.ipstack.com/{ip_destino}?access_key={api_key}"
-        response_destino = requests.get(api_url_destino)
-        result_destino = response_destino.json()
-
-        api_url_origen = f"http://api.ipstack.com/{ip_origen}?access_key={api_key}"
-        response_origen = requests.get(api_url_origen)
-        result_origen = response_origen.json()
+        # Utilizar la API de IPinfo para obtener información de geolocalización
+        result_destino = ipinfo_handler.getDetails(ip_destino).all
+        result_origen = ipinfo_handler.getDetails(ip_origen).all
 
         # Extraer las coordenadas de latitud y longitud
         lat_destino = result_destino.get('latitude')
@@ -63,12 +58,12 @@ for index, row in df.iterrows():
             folium.PolyLine([coord_pair[1], coord_pair[0]], color='green').add_to(m)
 
         # Guardar el mapa como un archivo HTML
-        m.save(r"E:\Cursos\BC_Data_Science\Repositorio\ONLINE_DS_THEBRIDGE_V\proyecto EDA\mapa_ips.html")
+        m.save(r'E:\Cursos\BC_Data_Science\Repositorio\ONLINE_DS_THEBRIDGE_V\proyecto EDA\mapa_ips.html')
 
         print("Procesadas {} IPs. El programa se detendra.".format(index))
         break
 
-    # Pausa de 90 segundos entre iteraciones
+    # Pausa de 30 segundos entre iteraciones
     time.sleep(30)
         
 
